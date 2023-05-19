43c2f5e079e35fa1ee98b1ea62847b33572aaacd
Nitu170287 <108494260+Nitu170287@users.noreply.github.com>
2023-05-19 18:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function getSize(){
+	var x = document.documentElement.clientWidth;
+	var y = document.documentElement.clientHeight;
+
+	document.getElementById("sizeInfo").innerHTML = "<h1>Width: " + x + " and Height: " + y + "</h1>"
+}
